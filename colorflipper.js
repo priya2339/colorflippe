e1e637a4268367b4b2c6f0e771a0d8acd2b06244
priya2339 <pc2315168@gmail.com>
2024-08-19 17:05:05 +0530

    Add files via upload

---
+++ colorflipper.js
@@ -0,0 +1,37 @@
+
+  function changecolor(){
+
+    document.querySelector('.box').style.backgroundColor = 'red'
+
+  }
+
+
+  function changecolor1(){
+
+    document.querySelector('.box').style.backgroundColor = 'blue'
+
+  }
+
+  function changecolor2(){
+
+    document.querySelector('.box').style.backgroundColor = 'orange'
+
+  }
+
+  function changecolor3(){
+
+    document.querySelector('.box').style.backgroundColor = 'black'
+
+  }
+
+  function changecolor4(){
+
+    document.querySelector('.box').style.backgroundColor = 'green'
+
+  }
+
+  function changecolor5(){
+
+    document.querySelector('.box').style.backgroundColor = 'yellow'
+
+  }
